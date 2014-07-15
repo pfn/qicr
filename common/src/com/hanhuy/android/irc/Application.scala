fd7ba7dcc2f25530606adc294ee2bc44770049ad
Perry Nguyen <pfnguyen@hanhuy.com>
2014-07-14 17:01:39 -0700

    update to actually make acra work

--- common/src/com/hanhuy/android/irc/Application.scala
+++ common/src/com/hanhuy/android/irc/Application.scala
@@ -6,7 +6,8 @@ import org.acra.annotation.ReportsCrashes
 /**
  * @author pfnguyen
  */
-@ReportsCrashes(formKey = "", formUri = "http://hanhuy-acra.appspot.com/api/crashreport")
+@ReportsCrashes(formKey = "",
+  formUri = "http://hanhuy-acra.appspot.com/api/crashreport")
 class Application extends android.app.Application {
   override def onCreate() {
     super.onCreate()
