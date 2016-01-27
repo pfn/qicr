ee8506967d7e01b2fdc5045417c20a918851ad02
Perry Nguyen <pfnguyen@hanhuy.com>
2016-01-27 10:08:31 -0800

    Refine ActivityResultManager

--- src/main/scala/com/hanhuy/android/irc/ActivityResultManager.scala
+++ src/main/scala/com/hanhuy/android/irc/ActivityResultManager.scala
@@ -10,16 +10,20 @@ import scala.concurrent.{Future, Promise}
 /**
   * @author pfnguyen
   */
-trait ActivityResultManager extends Activity {
+
+trait BaseResultManager {
   case object ActivityResultCancel extends Exception
+  case class ActivityResultProcessor[A,B](startActivityForResult: (Intent, Int) => A, onActivityResult: (Int, Int, Intent) => B)
+
+  def activityFragmentWrapper: ActivityResultProcessor[_,_]
   private[this] var _requestCode = 0
-  def requestCode = {
+  final def requestCode = {
     _requestCode = _requestCode + 1
     _requestCode % 0xff
   }
   private[this] val arlog = Logcat("ActivityResultManager")
   private[this] var results = Map.empty[Int,Promise[Intent]]
-  final override def onActivityResult(request: Int,
+  protected def processActivityResult(request: Int,
                                       resultCode: Int,
                                       data: Intent): Unit = {
     if (results.contains(request)) {
@@ -30,7 +34,7 @@ trait ActivityResultManager extends Activity {
       }
       results -= request
     } else {
-      super.onActivityResult(request, resultCode, data)
+      activityFragmentWrapper.onActivityResult(request, resultCode, data)
       arlog.w(s"Request code $request not found: " + data)
     }
   }
@@ -38,39 +42,21 @@ trait ActivityResultManager extends Activity {
     val req = requestCode
     val p = results.getOrElse(req, Promise[Intent]())
     results += ((req, p))
-    super.startActivityForResult(intent, req)
+    activityFragmentWrapper.startActivityForResult(intent, req)
     p.future
   }
 }
-trait FragmentResultManager extends Fragment {
-  case object ActivityResultCancel extends Exception
-  private[this] var _requestCode = 0
-  def requestCode = {
-    _requestCode = _requestCode + 1
-    _requestCode % 0xff
-  }
-  private[this] val arlog = Logcat("ActivityResultManager")
-  private[this] var results = Map.empty[Int,Promise[Intent]]
-  final override def onActivityResult(request: Int,
-                                      resultCode: Int,
-                                      data: Intent): Unit = {
-    if (results.contains(request)) {
-      if (resultCode != Activity.RESULT_OK) {
-        results(request).failure(ActivityResultCancel)
-      } else {
-        results(request).success(data)
-      }
-      results -= request
-    } else {
-      super.onActivityResult(request, resultCode, data)
-      arlog.w(s"Request code $request not found: " + data)
-    }
-  }
-  final def requestActivityResult(intent: Intent): Future[Intent] = {
-    val req = requestCode
-    val p = results.getOrElse(req, Promise[Intent]())
-    results += ((req, p))
-    super.startActivityForResult(intent, req)
-    p.future
-  }
+trait ActivityResultManager extends Activity with BaseResultManager {
+  final def activityFragmentWrapper = ActivityResultProcessor(
+    (i,r) => startActivityForResult(i, r),
+    (req, res, data) => super.onActivityResult(req, res, data))
+
+  override def onActivityResult(requestCode: Int, resultCode: Int, data: Intent) = processActivityResult(requestCode, resultCode, data)
+}
+trait FragmentResultManager extends Fragment with BaseResultManager {
+  final def activityFragmentWrapper = ActivityResultProcessor(
+    (i,r) => startActivityForResult(i, r),
+    (req, res, data) => super.onActivityResult(req, res, data))
+
+  override def onActivityResult(requestCode: Int, resultCode: Int, data: Intent) = processActivityResult(requestCode, resultCode, data)
 }
