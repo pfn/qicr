c3eb2ca2db37e2225640c3c3f5225c6d46a3f52b
Perry Nguyen <pfnguyen@hanhuy.com>
2016-02-16 21:45:49 -0800

    Initial attempt at creating a pure Activity and Fragment API

--- src/main/scala/com/hanhuy/android/irc/LogcatActivity.scala
+++ src/main/scala/com/hanhuy/android/irc/LogcatActivity.scala
@@ -3,13 +3,14 @@ package com.hanhuy.android.irc
 import android.graphics.Typeface
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.{LinearLayoutManager, RecyclerView}
+import android.support.v7.widget.{Toolbar, LinearLayoutManager, RecyclerView}
 import android.view.ViewGroup
 import android.widget.{TextView, LinearLayout}
 import com.hanhuy.android.irc.model.{MessageAdapter, RingBuffer}
 
 import scala.concurrent.Future
 import sys.process._
+import java.lang.{Process => _}
 
 import com.hanhuy.android.common._
 import com.hanhuy.android.appcompat.extensions._
@@ -19,12 +20,10 @@ import Futures._
 import SpannedGenerator._
 import ViewGroup.LayoutParams._
 
-
 /**
   * @author pfnguyen
   */
-class LogcatActivity extends AppCompatActivity {
-  var logcatProcess = Option.empty[sys.process.Process]
+class LogcatActivity extends AppCompatActivity with PureActivity[Option[Process]] {
   val LOG_LINE = """^([A-Z])/(.+?)\( *(\d+)\): (.*?)$""".r
   val buffersize = 1024
   lazy val toolbar = newToolbar
@@ -35,52 +34,53 @@ class LogcatActivity extends AppCompatActivity {
     r
   }
   lazy val layout = l[LinearLayout](
-    toolbar.! >>= lp(MATCH_PARENT, WRAP_CONTENT),
+    toolbar.!  >>= lp(MATCH_PARENT, WRAP_CONTENT),
     recycler.! >>= lp(MATCH_PARENT, 0, 1)
   ) >>= vertical
 
-  override def onCreate(savedInstanceState: Bundle) = {
-    setTheme(if (Settings.get(Settings.DAYNIGHT_MODE)) R.style.SetupTheme_Light else R.style.SetupTheme_Dark)
-    super.onCreate(savedInstanceState)
-    setSupportActionBar(toolbar)
-    getSupportActionBar.setTitle("Logcat")
-    toolbar.setNavigationIcon(resolveAttr(R.attr.qicrCloseIcon, _.resourceId))
-    toolbar.navigationOnClick0(finish())
-    setContentView(layout.perform())
-  }
+  override def initialState(b: Option[Bundle]) = None
 
-  override def onStart() = {
-    super.onStart()
-    var buffering = true
-    val logcat = "logcat" :: "-v" :: "brief" :: Nil
-    val lineLogger = new ProcessLogger {
-      override def out(s: => String) = addLine(s)
-      override def buffer[T](f: => T) = f
-      override def err(s: => String) = addLine(s)
-      def addLine(line: String) = line match {
-        case LOG_LINE(level, tag, pid, msg) =>
-          if (tag != "ResourceType") UiBus.run {
-            val c = Adapter.getItemCount // store in case at max items already
-            Adapter.buffer += LogEntry(tag, level, msg)
-            Adapter.notifyItemInserted(math.min(buffersize, c + 1))
-            if (!buffering)
-              recycler.smoothScrollToPosition(Adapter.getItemCount)
-          }
-        case _ =>
+  override def applyState[T](s: ActivityState[T]) = s match {
+    case OnCreate(_) => s(IO {
+      setTheme(if (Settings.get(Settings.DAYNIGHT_MODE)) R.style.SetupTheme_Light else R.style.SetupTheme_Dark)
+      toolbar.setTitle("Logcat")
+      toolbar.setNavigationIcon(resolveAttr(R.attr.qicrCloseIcon, _.resourceId))
+      toolbar.navigationOnClick0(finish())
+      setContentView(layout.perform())
+    })
+    case OnStart(_) => s.applyState(IO {
+      var buffering = true
+      val logcat = "logcat" :: "-v" :: "brief" :: Nil
+      val lineLogger = new ProcessLogger {
+        override def out(s: => String) = addLine(s)
+        override def buffer[X](f: => X) = f
+        override def err(s: => String) = addLine(s)
+
+        def addLine(line: String) = line match {
+          case LOG_LINE(level, tag, pid, msg) =>
+            if (tag != "ResourceType") UiBus.run {
+              val c = Adapter.getItemCount // store in case at max items already
+              Adapter.buffer += LogEntry(tag, level, msg)
+              Adapter.notifyItemInserted(math.min(buffersize, c + 1))
+              if (!buffering)
+                recycler.smoothScrollToPosition(Adapter.getItemCount)
+            }
+          case _ =>
+        }
       }
-    }
-    logcatProcess = logcat.run(lineLogger).?
-    Future {
-      Thread.sleep(500)
-      buffering = false
-    } onSuccessMain { case _ =>
+      Future {
+        Thread.sleep(500)
+        buffering = false
+      } onSuccessMain { case _ =>
         recycler.scrollToPosition(Adapter.getItemCount - 1)
-    }
-  }
-
-  override def onStop() = {
-    super.onStop()
-    logcatProcess.foreach(_.destroy())
+      }
+      logcat.run(lineLogger).?
+    })
+    case OnStop(proc) => s.applyState(IO {
+      proc.foreach(_.destroy())
+      None
+    })
+    case x => defaultApplyState(x)
   }
 
   case class LogEntry(tag: String, level: String, msg: String)
