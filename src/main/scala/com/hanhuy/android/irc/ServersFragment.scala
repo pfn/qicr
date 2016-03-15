8b8d531ec40779d4d2699c4a9f26afd73f30e583
Perry Nguyen <pfnguyen@hanhuy.com>
2016-03-14 17:41:31 -0700

    Fix random crash

--- src/main/scala/com/hanhuy/android/irc/ServersFragment.scala
+++ src/main/scala/com/hanhuy/android/irc/ServersFragment.scala
@@ -59,21 +59,23 @@ class ServersFragment extends Fragment with EventBus.RefOwner {
     super.onAttach(context)
     UiBus += {
       case BusEvent.ServerAdded(server)   =>
-        emptyView.setVisibility(View.GONE)
-        recycler.setVisibility(View.VISIBLE)
-        if (getActivity != null)
+        if (getActivity != null) {
+          emptyView.setVisibility(View.GONE)
+          recycler.setVisibility(View.VISIBLE)
           adapter.notifyItemInserted(manager.getServers.indexOf(server))
+        }
       case BusEvent.ServerChanged(server) =>
         if (getActivity != null)
           adapter.notifyItemChanged(manager.getServers.indexOf(server))
       case BusEvent.ServerRemoved(server) =>
-        if (Config.servers.now.isEmpty) {
-          emptyView.setVisibility(View.VISIBLE)
-          recycler.setVisibility(View.GONE)
-        }
-        selectedItem = None
-        if (getActivity != null)
+        if (getActivity != null) {
+          if (Config.servers.now.isEmpty) {
+            emptyView.setVisibility(View.VISIBLE)
+            recycler.setVisibility(View.GONE)
+          }
+          selectedItem = None
           adapter.notifyDataSetChanged()
+        }
     }
   }
 
